daf5efa989b9b5136fd8fb448f4d1e469d1a47c0
Mark Szymczyk <mark@swiftdevjournal.com>
2022-06-27 20:32:54 -0400

    Add properties for the bill amount, the tip percentage, and the tip amount.

--- ContentView.swift
+++ ContentView.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var billAmount: Double = 0
+    @State var tipPercentage: Int = 20
+    @State var tipAmount: Double = 0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
