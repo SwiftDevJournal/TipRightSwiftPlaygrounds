7c05192d1abe88ae4ae9ea7fd76a6068bdc2b7ae
Mark Szymczyk <mark@swiftdevjournal.com>
2022-06-27 22:00:41 -0400

    Make the properties private variables. This is standard SwiftUI practice as @State variables are local to the view, with other views using @Binding to access the @State variables.

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var billAmount: Double = 0
-    @State var tipPercentage: Int = 20
-    @State var tipAmount: Double = 0
+    @State private var billAmount: Double = 0
+    @State private var tipPercentage: Int = 20
+    @State private var tipAmount: Double = 0
     
     var body: some View {
         VStack {
