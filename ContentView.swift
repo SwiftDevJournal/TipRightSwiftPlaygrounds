d986a43c6cbea487121b166fdb31cd937ea6de24
Mark Szymczyk <mark@swiftdevjournal.com>
2022-07-05 14:39:03 -0400

    Add a Calculate Tip title to the content view.

--- ContentView.swift
+++ ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Text("Calculate Tip")
+                .font(.title)
+                .padding(.vertical)
             HStack {
                 Spacer()
                 Text("Bill Amount:")
